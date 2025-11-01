2aded8d75434704633d898e853484c48028469f3
farrukh806 <farrukhjamal806@gmail.com>
2025-11-01 13:54:22 +0530

    Fix output path in webpack configuration to correctly reference the dist directory

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -11,7 +11,7 @@ const prodConfig = {
     output: {
         filename: "[name].[contenthash].js",
         publicPath: "/container/latest/",
-        path: path.resolve(__dirname, 'dist/container/latest'),
+        path: path.resolve(__dirname, '../dist/container/latest'),
     },
     plugins: [
         new ModuleFederationPlugin({
